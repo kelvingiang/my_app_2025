aed1ee28102956f60c9633fac8812faf23787faf
Kelvin Giang <giaminh0265@yahoo.com>
2025-06-17 09:30:37 +0700

    doi ten API_local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment ={
     API_URL: 'https://app.esgservice.com.vn/',
-    API_LOCAL : 'http://localhost/ctchn/api/',
+    API_LOCAL : 'http://localhost/service_wordpress_app/api/',
     API_GUEST : 'http://localhost/service_my_app/',
     // API_GUEST : 'https://guest.esgservice.com.vn/',
     API_CODE  :  'MA_BAO_MAT_GET_DATA',
