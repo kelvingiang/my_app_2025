3cff0142b812c9f9dc059220a42a7e7c209bf6db
kelvin Giang <52771444+giaminh0256@users.noreply.github.com>
2025-06-19 13:55:08 +0700

    chinh sua thich hop tu macbook

--- src/app/guest/guest-new/guest-new.component.ts
+++ src/app/guest/guest-new/guest-new.component.ts
@@ -3,6 +3,7 @@ import { GuestService } from "./../../service/guest.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import {FormControl} from '@angular/forms';
 
+
 @Component({
   selector: "app-guest-new",
   templateUrl: "./guest-new.component.html",
@@ -17,6 +18,7 @@ export class GuestNewComponent implements OnInit {
     phone: "",
     date: "",
   };
+
   selectedFile: File | null = null;
 
   constructor(
@@ -42,30 +44,23 @@ export class GuestNewComponent implements OnInit {
     formData.append("position", this.formData.position);
     formData.append("email", this.formData.email);
     formData.append("phone", this.formData.phone);
-    // formData.append("create_date", this.getCurrentDate());
 
     if (this.selectedFile) {
       formData.append('file', this.selectedFile);
- //   } else {
-      // console.warn('尚未選擇檔案');
-      //return;
     }
-    // formData.append("file", this.selectedFile);
-    // 加入當天的日期到 formData 中
-    // this.formData.date = this.getCurrentDate();
-    // this.formData.file = this.selectedFile;
-    // console.log(formData);
+   
     this._guestService.addGuest(formData).subscribe({
-      next: (response) => {
-        console.log(response);
-        // console.log('User insert successfully:', response);
-        alert("insert successful!");
-        this._router.navigate(["guest"]);
+    next: (response) => {
+      console.log(response);
+      alert("insert successful!");
+      this._router.navigate(["guest"]);
       },
-      error: (err) => {
-        console.error('Error insert user:', err);
-        alert("insert failed! ..");
-      },
+  error: (err) => {
+      console.error('Error insert user:', err);
+      alert("insert failed!");
+      console.error('錯誤物件:', err);
+      console.error('回應內容:', err.error); // 建議加上這行，沒錯
+      }
     });
   }
 
